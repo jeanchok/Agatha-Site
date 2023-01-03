b0bd4c7c738085cc92d7c585165a0e19e95e9c5b
jeanchok <48122647+jeanchok@users.noreply.github.com>
2023-01-03 17:18:00 +0100

    comment carroussel

--- frontend_agatha/src/Components/Actus.jsx
+++ frontend_agatha/src/Components/Actus.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useContext } from 'react';
 import env from "react-dotenv";
 import useFetch from '../Hooks/useFetch'
 import { Link } from 'react-router-dom'
-import Carousel from 'carousel-react-rcdev'
+//import Carousel from 'carousel-react-rcdev'
 import { InView } from 'react-intersection-observer';
 import { MyContext } from '../Hooks/useContext';
 
@@ -23,7 +23,7 @@ const Actus = () => {
             <InView as="div" className='actus' onChange={(inView, entry) => { if (inView) { setNavActive('actus') } }} >
                 <h2>LES ACTUS</h2>
                 <div className='actusContainer'>
-                    <Carousel>
+                    {/* <Carousel>
                         {data.data.map(actus => (
                             <div key={actus.id} className="mycard">
                                 <div className='mycard__text'>
@@ -33,7 +33,7 @@ const Actus = () => {
                                 <img src={API_URL + actus.attributes.imageActu.data.attributes.url} alt="" />
                             </div>
                         ))}
-                    </Carousel>
+                    </Carousel> */}
                 </div>
                 <div className='linkContainer'>
                     <a className='linkContainer__linkButton glow-on-hover' href="/">
