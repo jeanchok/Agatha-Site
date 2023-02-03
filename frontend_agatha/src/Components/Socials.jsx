bc0b9c798803f8b6f44741a7d714542ff8c312a8
jeanchok <48122647+jeanchok@users.noreply.github.com>
2023-02-03 17:38:57 +0100

    links to socials

--- frontend_agatha/src/Components/Socials.jsx
+++ frontend_agatha/src/Components/Socials.jsx
@@ -12,10 +12,10 @@ const Socials = () => {
         <div className="wrapper">
             <h4>SUIVEZ-NOUS</h4>
             <div className='socialIcone__Container'>
-                <a href="https://instagram.com/" target="_instagram" aria-label="Suivez-nous sur instagram">
+                <a href="https://instagram.com/agatha.asso?igshid=YmMyMTA2M2Y=" target="_instagram" aria-label="Suivez-nous sur instagram">
                     <FontAwesomeIcon icon={faInstagram} size="3x" className='socialIcone__Container--icone' />
                 </a>
-                <a href="https://facebook.com/" target="_facebook" aria-label="Suivez-nous sur facebook">
+                <a href="https://www.facebook.com/profile.php?id=100089857322310" target="_facebook" aria-label="Suivez-nous sur facebook">
                     <FontAwesomeIcon icon={faFacebook} size="3x" className='socialIcone__Container--icone' />
                 </a>
             </div>
